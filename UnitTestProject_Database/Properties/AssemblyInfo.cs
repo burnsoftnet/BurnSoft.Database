ce7f8bb52c7ee21dbe0920715d08b33b50c4189a
Joe Mireles <bs.github@burnsoft.net>
2019-03-17 00:20:40 -0400

    #5 Started Unit Test

---
+++ UnitTestProject_Database/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_Database")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_Database")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("133fcbad-d906-4d83-908e-6ad2081e394a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject_Database/UnitTest_MSAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Database
+{
+    [TestClass]
+    public class UnitTest_MSAccess
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
