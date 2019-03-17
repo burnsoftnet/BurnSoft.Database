6856b37f77a040d54f4b017474ea3a5236bd6809
Joe Mireles <bs.github@burnsoft.net>
2019-03-16 23:55:32 -0400

    #1 Updated Project description

--- BurnSoft.Database/Properties/AssemblyInfo.cs
+++ BurnSoft.Database/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnSoft.Database")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("General Functions to Connect to different types of databases along with the ability to run SQL commands and return data.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Database")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © www.burnsoft.net 2019")]
+[assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
