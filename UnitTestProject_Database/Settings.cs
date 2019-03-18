85aee37b9662fc22157736a10937dd84e9bbffdd
Joe Mireles <bs.github@burnsoft.net>
2019-03-18 08:08:25 -0400

    #5 Added setting section in unit test for Access Database Testing

---
+++ UnitTestProject_Database/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject_Database
+{
+    /// <summary>
+    /// Class Settings for Unit Tests
+    /// </summary>
+    public class Settings
+    {
+        /// <summary>
+        /// Class AccessDatabase Settings for unit tests
+        /// </summary>
+        public class AccessDatabase
+        {
+            /// <summary>
+            /// The database name
+            /// </summary>
+            public const string DatabaseName = "";
+            /// <summary>
+            /// The database path
+            /// </summary>
+            public const string DatabasePath = "";
+            /// <summary>
+            /// The database password
+            /// </summary>
+            public const string DatabasePassword = "";
+
+        }
+    }
+}
