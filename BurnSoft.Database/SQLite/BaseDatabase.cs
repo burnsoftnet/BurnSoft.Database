bfe275ec50f68dbe10a6f249595f5213e153b9de
Joe M <joe.mireles@burnsoft.net>
2019-03-18 09:29:32 -0400

    #3 Create Class for basic sqlite functions

---
+++ BurnSoft.Database/SQLite/BaseDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BurnSoft.Database.SQLite
+{
+    /// <summary>
+    /// Class BaseDatabase is used for creating the base database if it doesn't exist and the ability to put in the versioning of the
+    /// database in ia table or update the versioning
+    /// </summary>
+    public class BaseDatabase
+    {
+
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string ClassLocation = "BurnSoft.Database.MSAccess.MSAccessDatabase";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, Exception e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, InvalidCastException e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, ArgumentNullException e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        #endregion
+
+    }
+}
