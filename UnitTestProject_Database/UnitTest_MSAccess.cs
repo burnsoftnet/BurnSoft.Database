ffd8201c08b1d7f375eab1dcb723539cd751b44c
Joe M <joe.mireles@burnsoft.net>
2019-03-18 08:31:50 -0400

    #5 Added New tests to MSAccess unit test

--- UnitTestProject_Database/UnitTest_MSAccess.cs
+++ UnitTestProject_Database/UnitTest_MSAccess.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BurnSoft.Database;
 
 namespace UnitTestProject_Database
 {
     [TestClass]
     public class UnitTest_MSAccess
     {
+        private string errOut;
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionStringWithPassword.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_ConnectionStringWithPassword()
         {
+            string value = BurnSoft.Database.MSAccess.MSAccessDatabase.ConnectionString(Settings.AccessDatabase.DatabasePath, Settings.AccessDatabase.DatabaseName, Settings.AccessDatabase.DatabasePassword, out errOut);
+            General.HasValue(value, errOut);
+        }
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionStringWithOutPassword.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_ConnectionStringWithOutPassword()
+        {
+            string value = BurnSoft.Database.MSAccess.MSAccessDatabase.ConnectionString(Settings.AccessDatabase.DatabasePath, Settings.AccessDatabase.DatabaseName,"", out errOut);
+            General.HasValue(value, errOut);
+        }
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionOLEStringWithPassword.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_ConnectionOLEStringWithPassword()
+        {
+            string value = BurnSoft.Database.MSAccess.MSAccessDatabase.ConnectionStringOLE(Settings.AccessDatabase.DatabasePath, Settings.AccessDatabase.DatabaseName, Settings.AccessDatabase.DatabasePassword, out errOut);
+            General.HasValue(value, errOut);
+        }
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionOLEStringWithOutPassword.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_ConnectionOLEStringWithOutPassword()
+        {
+            string value = BurnSoft.Database.MSAccess.MSAccessDatabase.ConnectionStringOLE(Settings.AccessDatabase.DatabasePath, Settings.AccessDatabase.DatabaseName, "", out errOut);
+            General.HasValue(value, errOut);
         }
     }
 }
