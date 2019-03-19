b4bc26e7b8a7599d2db8f2607b8aa1a52ef7d98b
Joe Mireles <bs.github@burnsoft.net>
2019-03-19 07:52:59 -0400

    #3 Created new class to handle the data

---
+++ BurnSoft.Database/SQLite/SQLiteDataManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace BurnSoft.Database.SQLite
+{
+    /// <summary>
+    /// Class SQLiteDataManagement handles the ability to read and write data to the SQLite Database.
+    /// </summary>
+    public class SQLiteDataManagement
+    {
+
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string ClassLocation = "BurnSoft.Database.SQLite.SQLiteDataManagement";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, Exception e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, InvalidCastException e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        /// <summary>
+        /// Errors the message.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        /// <param name="FunctionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string location, string FunctionName, ArgumentNullException e) => "{ClassLocation}.{FunctionName} - {e.Message.ToString()}";
+        #endregion
+
+    }
+}
