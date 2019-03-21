a839ede65fdad681f55e220358a76f480468b302
Joe M <joe.mireles@burnsoft.net>
2019-03-21 09:48:44 -0400

    #4 Created class to MySQl database

---
+++ BurnSoft.Database/MySQL/MySQLDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Database.MySQL
+{
+    /// <summary>
+    /// Class MySQLDatabase handles connections to the Mysql database as well as some simple queries to the database as needed
+    /// </summary>
+    public class MySQLDatabase
+    {
+
+    }
+}
