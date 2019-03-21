9f79a79effdcb33ab425554744f358ed698c334a
Joe M <joe.mireles@burnsoft.net>
2019-03-21 09:13:24 -0400

    #6 #6 Added comments to unit test

--- UnitTestProject_Database/UnitTest_SQLite_SQLiteDataManagement.cs
+++ UnitTestProject_Database/UnitTest_SQLite_SQLiteDataManagement.cs
@@ -5,102 +5,126 @@ using System.Data;
 using System.Diagnostics;
 
 namespace UnitTestProject_Database
+
+    /// <summary>
+    /// Defines test class UnitTest_SQLite_SQLiteDataManagement.
+    /// </summary>
+[TestClass]
+public class UnitTest_SQLite_SQLiteDataManagement
 {
-    [TestClass]
-    public class UnitTest_SQLite_SQLiteDataManagement
+    private string errOut;
+    /// <summary>
+    /// Tests the initialize.
+    /// </summary>
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private string errOut;
-        [TestInitialize]
-        public void TestInitialize()
+        bool value = BaseDatabase.CreateStarterDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
+        if (errOut.Length > 0)
         {
-            bool value = BaseDatabase.CreateStarterDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
-            if (errOut.Length > 0)
-            {
-                General.HasTrueValue(value, errOut);
-            }
-        }
-
-        [TestMethod]
-        public void TestMethod_ConnectDB()
-        {
-            SQLiteDataManagement obj = new SQLiteDataManagement();
-            bool value = obj.ConnectDB(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
             General.HasTrueValue(value, errOut);
         }
-
-        [TestMethod]
-        public void TestMethod_RunQuery()
-        {
-            string sql = "INSERT INTO DB_Version (version) VALUES (0);";
-            bool value = SQLiteDataManagement.RunQuery(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
-            General.HasTrueValue(value, errOut);
-        }
-
-        [TestMethod]
-        public void TestMethod_HasData()
-        {
-            string sql = "select * from DB_Version;";
-            bool value = SQLiteDataManagement.HasData(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
-            General.HasTrueValue(value, errOut);
-        }
-
-        [TestMethod]
-        public void TestMethod_GetDataBySQL()
-        {
-            string sql = "select * from DB_Version;";
-            DataTable dt = SQLiteDataManagement.GetDataBySQL(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
-            bool HasData = false;
-            if (errOut.Length == 0)
-            {
-                HasData = (dt.Rows.Count > 0);
-                foreach (DataRow dr in dt.Rows)
-                {
-                    Debug.Print("{0}", dr["id"].ToString());
-                    Debug.Print("{0}", dr["version"].ToString());
-                    Debug.Print("{0}", dr["dt"].ToString());
-                    Debug.Print("");
-                }
-            }
-            General.HasTrueValue(HasData, errOut);
-        }
-
-        [TestMethod]
-        public void TestMethod_CleanDB()
-        {
-            bool value = SQLiteDataManagement.CleanDB(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
-            General.HasTrueValue(value, errOut);
-        }
-        //GetSingleValueFromDatabase
-        [TestMethod]
-        public void TestMethod_GetSingleValueFromDatabase_String()
-        {
-            string sql = "select * from DB_Version order by dt desc limit 1;";
-            string value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "dt", "", out errOut);
-            General.HasValue(value, errOut);
-        }
-        [TestMethod]
-        public void TestMethod_GetSingleValueFromDatabase_integer()
-        {
-            string sql = "select * from DB_Version order by dt desc limit 1;";
-            int value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "id", 0, out errOut);
-            bool HasValue = (value > 0);
-            Debug.Print("Value Returned is: {0}", value);
-            General.HasTrueValue(HasValue, errOut);
-        }
-        [TestMethod]
-        public void TestMethod_GetSingleValueFromDatabase_Double()
-        {
-            string sql = "select * from DB_Version order by dt desc limit 1;";
-            double value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "version", 0.0, out errOut);
-            bool HasValue = (value > 0);
-            Debug.Print("Value Returned is: {0}", value);
-            General.HasTrueValue(HasValue, errOut);
-        }
-        /*
-         [TestMethod]
-        public void TestMethod_()
-        {
-        }
-         */
     }
+    /// <summary>
+    /// Defines the test method TestMethod_ConnectDB.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_ConnectDB()
+    {
+        SQLiteDataManagement obj = new SQLiteDataManagement();
+        bool value = obj.ConnectDB(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
+        General.HasTrueValue(value, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_RunQuery.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_RunQuery()
+    {
+        string sql = "INSERT INTO DB_Version (version) VALUES (0);";
+        bool value = SQLiteDataManagement.RunQuery(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
+        General.HasTrueValue(value, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_HasData.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_HasData()
+    {
+        string sql = "select * from DB_Version;";
+        bool value = SQLiteDataManagement.HasData(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
+        General.HasTrueValue(value, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_GetDataBySQL.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_GetDataBySQL()
+    {
+        string sql = "select * from DB_Version;";
+        DataTable dt = SQLiteDataManagement.GetDataBySQL(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, out errOut);
+        bool HasData = false;
+        if (errOut.Length == 0)
+        {
+            HasData = (dt.Rows.Count > 0);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Debug.Print("{0}", dr["id"].ToString());
+                Debug.Print("{0}", dr["version"].ToString());
+                Debug.Print("{0}", dr["dt"].ToString());
+                Debug.Print("");
+            }
+        }
+        General.HasTrueValue(HasData, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_CleanDB.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_CleanDB()
+    {
+        bool value = SQLiteDataManagement.CleanDB(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, out errOut);
+        General.HasTrueValue(value, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_GetSingleValueFromDatabase_String.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_GetSingleValueFromDatabase_String()
+    {
+        string sql = "select * from DB_Version order by dt desc limit 1;";
+        string value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "dt", "", out errOut);
+        General.HasValue(value, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_GetSingleValueFromDatabase_integer.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_GetSingleValueFromDatabase_integer()
+    {
+        string sql = "select * from DB_Version order by dt desc limit 1;";
+        int value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "id", 0, out errOut);
+        bool HasValue = (value > 0);
+        Debug.Print("Value Returned is: {0}", value);
+        General.HasTrueValue(HasValue, errOut);
+    }
+    /// <summary>
+    /// Defines the test method TestMethod_GetSingleValueFromDatabase_Double.
+    /// </summary>
+    [TestMethod]
+    public void TestMethod_GetSingleValueFromDatabase_Double()
+    {
+        string sql = "select * from DB_Version order by dt desc limit 1;";
+        double value = SQLiteDataManagement.GetSingleValueFromDatabase(Settings.SQLiteDatabase.StarterDatabaseNameAndPath, sql, "version", 0.0, out errOut);
+        bool HasValue = (value > 0);
+        Debug.Print("Value Returned is: {0}", value);
+        General.HasTrueValue(HasValue, errOut);
+    }
+    /*
+     [TestMethod]
+    public void TestMethod_()
+    {
+    }
+     */
+}
 }
