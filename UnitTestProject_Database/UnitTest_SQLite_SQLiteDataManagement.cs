3301fd6f1e6c7cbfe407690d1079dd8de8251739
Joe Mireles <bs.github@burnsoft.net>
2019-03-21 08:19:09 -0400

    #5 created unit test for sqlitedatamanagement funciton

---
+++ UnitTestProject_Database/UnitTest_SQLite_SQLiteDataManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Database
+{
+    [TestClass]
+    public class UnitTest_SQLite_SQLiteDataManagement
+    {
+        [TestMethod]
+        public void TestMethod_()
+        {
+        }
+
+        /*
+         [TestMethod]
+        public void TestMethod_()
+        {
+        }
+         */
+    }
+}
