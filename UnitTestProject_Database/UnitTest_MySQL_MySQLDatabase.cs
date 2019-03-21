7fa0558b0510826f060c0c3923cb4c036d478523
Joe M <joe.mireles@burnsoft.net>
2019-03-21 10:12:19 -0400

    #5 #4 Added unit test for MySQL

---
+++ UnitTestProject_Database/UnitTest_MySQL_MySQLDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BurnSoft.Database.MySQL;
+namespace UnitTestProject_Database
+{
+    [TestClass]
+    public class UnitTest_MySQL_MySQLDatabase
+    {
+        private string errOut;
+        [TestMethod]
+        public void TestMethod_ConnectionString()
+        {
+           
+        }
+
+        /*
+        [TestMethod]
+        public void TestMethod_()
+        {
+        }
+        */
+    }
+}
