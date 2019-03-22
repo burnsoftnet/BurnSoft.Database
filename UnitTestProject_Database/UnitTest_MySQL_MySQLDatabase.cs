602f70113b0532ea379f1c329b48b7406b856d5f
Joe M <joe.mireles@burnsoft.net>
2019-03-22 16:02:15 -0400

    #5 finished connection string test and added connect test

--- UnitTestProject_Database/UnitTest_MySQL_MySQLDatabase.cs
+++ UnitTestProject_Database/UnitTest_MySQL_MySQLDatabase.cs
@@ -10,9 +10,19 @@ namespace UnitTestProject_Database
         [TestMethod]
         public void TestMethod_ConnectionString()
         {
-           string value = MySQLDatabase.ConnectionString(Settings.MySQLDatabase.HOSTNAME, Settings.MySQLDatabase.UID, Settings.MySQLDatabase.PWD)
+            string value = MySQLDatabase.ConnectionString(Settings.MySQLDatabase.HOSTNAME, Settings.MySQLDatabase.UID, Settings.MySQLDatabase.PWD, Settings.MySQLDatabase.Database, out errOut);
+            General.HasValue(value, errOut);
         }
 
+        [TestMethod]
+        public void TestMethod_ConnectDB()
+        {
+            MySQLDatabase obj = new MySQLDatabase();
+            string connString = MySQLDatabase.ConnectionString(Settings.MySQLDatabase.HOSTNAME, Settings.MySQLDatabase.UID, Settings.MySQLDatabase.PWD, Settings.MySQLDatabase.Database, out errOut);
+            bool value = obj.ConnectDB(connString, out errOut);
+            obj.Close();
+            General.HasTrueValue(value, errOut);
+        }
         /*
         [TestMethod]
         public void TestMethod_()
