ce8467257df17f824c3b18fee5b2c7d408f3ff8a
Joe M <joe.mireles@burnsoft.net>
2019-03-22 16:13:17 -0400

    #5 Added RunQuery Unit test

--- UnitTestProject_Database/UnitTest_MySQL_MySQLDatabase.cs
+++ UnitTestProject_Database/UnitTest_MySQL_MySQLDatabase.cs
@@ -6,6 +6,9 @@ namespace UnitTestProject_Database
     [TestClass]
     public class UnitTest_MySQL_MySQLDatabase
     {
+        /// <summary>
+        /// The error out
+        /// </summary>
         private string errOut;
         [TestMethod]
         public void TestMethod_ConnectionString()
@@ -23,6 +26,16 @@ namespace UnitTestProject_Database
             obj.Close();
             General.HasTrueValue(value, errOut);
         }
+
+        [TestMethod]
+        public void TestMethod_RunQuery()
+        {
+            string sql = "CREATE TABLE `DB_Version` (`ID` int(11) NOT NULL AUTO_INCREMENT,`verNo` varchar(45) DEFAULT NULL,`dtUpdated` timestamp NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(`ID`),  UNIQUE KEY `ID_UNIQUE` (`ID`)) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;";
+            string connString = MySQLDatabase.ConnectionString(Settings.MySQLDatabase.HOSTNAME, Settings.MySQLDatabase.UID, Settings.MySQLDatabase.PWD, Settings.MySQLDatabase.Database, out errOut);
+            bool value = MySQLDatabase.RunQuery(connString, sql, out errOut);
+            General.HasTrueValue(value, errOut);
+        }
+
         /*
         [TestMethod]
         public void TestMethod_()
