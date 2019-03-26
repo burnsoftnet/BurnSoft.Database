61465c3e78972c288c2a40ab6d1b4cc4fd79a428
Joe Mireles <bs.github@burnsoft.net>
2019-03-26 16:11:16 -0400

    #8 Created class for MSSQL Database

---
+++ BurnSoft.Database/MSSQL/MSSQLDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Database.MSSQL
+{
+    /// <summary>
+    /// Class MSSQLDatabase handles connections and data for Microsoft SQL Server based database.
+    /// </summary>
+    public class MSSQLDatabase
+    {
+
+    }
+}
