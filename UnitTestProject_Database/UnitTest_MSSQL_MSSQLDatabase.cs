dd11fd2b38fb43f43970c66d238c3427474a2c21
Joe M <joe.mireles@burnsoft.net>
2019-03-27 10:52:55 -0400

    #5 Added tests for connection string

--- UnitTestProject_Database/UnitTest_MSSQL_MSSQLDatabase.cs
+++ UnitTestProject_Database/UnitTest_MSSQL_MSSQLDatabase.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using BurnSoft.Database.MSSQL;
 namespace UnitTestProject_Database
 {
     [TestClass]
     public class UnitTest_MSSQL_MSSQLDatabase
     {
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionString.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_ConnectionString()
         {
+            string connString = MSSQLDatabase.ConnectionString(Settings.MSSQLDatabase.server, "", Settings.MSSQLDatabase.database, Settings.MSSQLDatabase.UID, Settings.MSSQLDatabase.pwd);
+            General.HasValue(connString);
+        }
+        /// <summary>
+        /// Defines the test method TestMethod_ConnectionStringInstance.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_ConnectionStringInstance()
+        {
+            string connString = MSSQLDatabase.ConnectionString(Settings.MSSQLDatabase.server, "test", Settings.MSSQLDatabase.database, Settings.MSSQLDatabase.UID, Settings.MSSQLDatabase.pwd);
+            General.HasValue(connString);
         }
     }
 }
