7fbeeff0b57bdbf9dee28da049ddc1ff052a0d35
Joe M <joe.mireles@burnsoft.net>
2019-03-27 10:20:49 -0400

    #5 Added new unit test of MSSQL

---
+++ UnitTestProject_Database/UnitTest_MSSQL_MSSQLDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject_Database
+{
+    [TestClass]
+    public class UnitTest_MSSQL_MSSQLDatabase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
