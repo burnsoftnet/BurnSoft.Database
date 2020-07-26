54e2e098120de533f99ae3a2ebd29619bb4c9d67
Joe M <joe.mireles@burnsoft.net>
2020-07-26 17:50:26 -0400

    #9 Added add password to database function

--- BurnSoft.Database/MSAccess/OLEDB.cs
+++ BurnSoft.Database/MSAccess/OLEDB.cs
@@ -14,5 +14,50 @@ namespace BurnSoft.Database.MSAccess
     public class Oledb
     {
         //TODO Add code code to access oledb database for access databases
+        public static bool AddPasswordToDatabase(string path, string password, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                ADODB.Connection conn = new ADODB.Connection();
+                conn.Provider = "Microsoft.Jet.OLEDB.4.0";
+                conn.ConnectionString = $"Data Source={path}";
+                //conn.Properties["Jet OLEDB:Database Password"].Value = password;
+                conn.Mode = ADODB.ConnectModeEnum.adModeShareExclusive;
+                conn.Open();
+                string sql = $"ALTER DATABASE PASSWORD {password} NULL";
+                conn.Execute(sql, out _);
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                errOut = e.Message;
+            }
+            return bAns;
+        }
+
+        public static bool RemovePasswordToDatabase(string path, string password, out string errOut)
+        {
+            bool bAns = false;
+            errOut = @"";
+            try
+            {
+                ADODB.Connection conn = new ADODB.Connection();
+                conn.Provider = "Microsoft.Jet.OLEDB.4.0";
+                conn.ConnectionString = $"Data Source={path}";
+                //conn.Properties["Jet OLEDB:Database Password"].Value = password;
+                conn.Mode = ADODB.ConnectModeEnum.adModeShareExclusive;
+                conn.Open();
+                string sql = $"ALTER DATABASE PASSWORD {password} NULL";
+                conn.Execute(sql, out _);
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                errOut = e.Message;
+            }
+            return bAns;
+        }
     }
 }
