078f24e293f69e8a93b68824a155a7dcca479d9a
BurnSoft <bs.github@burnsoft.net>
2022-04-07 11:35:47 -0400

    #14  updated build settings

--- BurnSoft.Database/Properties/AssemblyInfo.cs
+++ BurnSoft.Database/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Database")]
-[assembly: AssemblyCopyright("Copyright © www.burnsoft.net 2019 - 2021")]
+[assembly: AssemblyCopyright("Copyright © www.burnsoft.net 2019 - 2022")]
 [assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.10")]
-[assembly: AssemblyFileVersion("1.0.0.10")]
+[assembly: AssemblyVersion("1.0.0.11")]
+[assembly: AssemblyFileVersion("1.0.0.11")]
