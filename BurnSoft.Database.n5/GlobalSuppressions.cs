427ba1497415b646d18defcff9eb62547a6fa6a2
BurnSoft <bs.github@burnsoft.net>
2022-04-07 13:55:14 -0400

    #12 added code

---
+++ BurnSoft.Database.n5/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:BurnSoft.Database.MSAccess.MsAccessDatabase.ClassLocation")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.ConnExec(System.String,System.String,System.String@)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.ConnExec(System.String,System.String,System.String@)~System.Boolean")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.ErrorMessage(System.String,System.ArgumentNullException)~System.String")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.ErrorMessage(System.String,System.InvalidCastException)~System.String")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.GetData(System.String,System.String,System.String@)~System.Data.DataTable")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.GetData(System.String,System.String,System.String@)~System.Data.DataTable")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.GetIDFromTableBasedOnTSQL(System.String,System.String,System.String,System.String@)~System.Int32")]
+[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Database.MSAccess.MsAccessDatabase.GetIDFromTableBasedOnTSQL(System.String,System.String,System.String,System.String@)~System.Int32")]
