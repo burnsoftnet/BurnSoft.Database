bdbbbeea92fa4934c47d9d38739edf748ec08574
BurnSoft <bs.github@burnsoft.net>
2026-02-02 08:28:00 -0500

    #14 Have the MSAccess tests passing, need to work on the rest

--- UnitTestProject_Database/Settings/Settings.cs
+++ UnitTestProject_Database/Settings/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace UnitTestProject_Database
             /// <summary>
             /// The database path
             /// </summary>
-            public const string DatabasePath = "C:\\test";
+            public static string DatabasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data");
             /// <summary>
             /// The database password
             /// </summary>
